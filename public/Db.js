21f98db38d9270b18d033520b6d903e211fab971
NeyNey <drfrankenstein123@gmail.com>
2020-11-24 11:49:16 -0600

    service worker in place

---
+++ public/Db.js
@@ -0,0 +1,52 @@
+const { response } = require("express");
+
+let db;
+const request = indexedDB.open("budget, 1");
+
+request.onupgraded = function(event) {
+    const db = event.target.result;
+    db.createObjectsStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess= function(event) {
+    db = event.target.result;
+    if (navigator.online) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("ya broke it" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.createObjectsStore("pending");
+    const getAll = store.getAll;
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    accept: "application/json, textplain, */*",
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json()
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            }));
+        }
+    }
+}
+
+function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+}
+
+window.addEventListener("online", checkDatabase);
---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/db.js",
+    "style.css"
+];
+
+const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v1";
+
+self.addEventListener("install", evt => {
+    evt.waitUntill(
+        caches.open(CACHE_NAME).then(chache => {
+            console.log("files pre-cached");
+            return caches.addAll(FILES_TO_CACHE).then((result) => {
+                console.log("result add all", result);
+            }).catch((err) => {
+                console.log("Error: ", err);
+            });
+        })
+    )
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+    evt.waitUntill(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("old cache data removed", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.ClientRectList.claim();
+})
+
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then((cache) => {
+                return fetch(evt.request).then((response) => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                }).catch((err) => {
+                    return cache.match(evt.request);
+                });
+            }).catch((err) => console.log(err))
+        );
+        return;
+    }
+    evt,respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(evt.request).then((response) => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
